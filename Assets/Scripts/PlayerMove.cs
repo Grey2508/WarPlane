b6ba27127cc4e71f936a554f61bba5a8131d404b
Grey2508 <grey-_@mail.ru>
2022-10-10 13:55:23 +0500

    Delete unusing usings

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Build.Reporting;
-using UnityEditorInternal;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerMove : MonoBehaviour
